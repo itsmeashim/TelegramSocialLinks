3b87dd0f0d8d0ba7fccd1d3ae417dc13e03bd9c2
itsmeashim <84639398+itsmeashim@users.noreply.github.com>
2024-03-20 16:09:59 +0000

    Fixed PeerInvalid

--- main.py
+++ main.py
@@ -8,7 +8,9 @@ import logging
 import os
 from dotenv import load_dotenv
 from ownserver_logger import send_exception_to_discord, send_message_to_discord
-from urllib.parse import urlparse
+import tldextract
+
+logging.basicConfig(level=logging.INFO)
 
 load_dotenv()
 
@@ -24,7 +26,7 @@ app = Client("my_account", session_string=session_string)
 @app.on_message(filters.chat(monitored_usernames))
 async def check_message(client, message):
     try:
-        log.info(f"New Message")
+        logging.info(f"New Message")
 
         message_text = message.text
         message_caption = message.caption
@@ -40,11 +42,18 @@ async def check_message(client, message):
         logging.info(f"URLs: {urls}")
 
         # Domains to filter out
-        filter_domains = ["twitter.com", "x.com", "discord.gg", "t.me", "instagram.com", "facebook.com", "reddit.com", "youtube.com", "tiktok.com","binance.com", "opensea.io", "rarible.com", "solsea.io", "solible.com", "solible.io", "wikipedia.com", "solscan.io", "birdeye.so", "dexscreener.com", "rugcheck.xyz", "tinyastro.io"]
+        filter_domains = ["twitter.com", "x.com", "discord.gg", "t.me", "instagram.com", "facebook.com", "reddit.com", "youtube.com", "tiktok.com","binance.com", "opensea.io", "rarible.com", "solsea.io", "solible.com", "solible.io", "wikipedia.com", "solscan.io", "birdeye.so", "dexscreener.com", "rugcheck.xyz", "tinyastro.io", "www.instagram.com", "medium.com", "wikipedia.org"]
 
         # Extracting hostnames and filtering
 
-        filtered_urls = [urlparse(url).hostname for url in urls if urlparse(url).hostname not in filter_domains]
+        urls = [tldextract.extract(url).registered_domain for url in urls]
+
+        try:
+            filtered_urls = [url for url in urls if url not in filter_domains]
+        except Exception as e:
+            print(e)
+            filtered_urls = []
+
         logging.info(f"Filtered URLs: {filtered_urls}")
         print(f"Filtered URLs: {filtered_urls}")
 
